a2c4e5a769cf2884b37e71bc09a70386013cf869
heyner31 <heyner2000_10@gmail.com>
2020-03-29 00:31:03 -0600

    CH

--- miweb/index.php
+++ miweb/index.php
@@ -1,14 +1,13 @@
-<?php 
+<!-- <?php echo "Hola chady, tu te ves bonita TQM"; ?> -->
 
-	echo "Hola profesor, soy heyner"; 
 
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>PROYECTO DE FUNDAMENTOS DE LA NUBE</title>
 </head>
 <body>
-
+	<center><img src="https://image.freepik.com/vector-gratis/lindo-gatito-corazon-kawaii-personaje_24640-40616.jpg"></center>
+	<center><h2>Hola chady, tu te ves bonita TQM</h2></center>
 </body>
 </html>
